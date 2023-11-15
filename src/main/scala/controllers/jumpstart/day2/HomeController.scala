29cccef0166606e22a727129756436e328ec5aa1
Watson Dinh <superusr.root@gmail.com>
2023-11-15 14:49:40 +0700

    impl

--- src/main/scala/controllers/jumpstart/day2/HomeController.scala
+++ src/main/scala/controllers/jumpstart/day2/HomeController.scala
@@ -1,11 +1,13 @@
 package controllers.jumpstart.day2
 
 import com.greenfossil.data.mapping.Mapping
-import com.greenfossil.jumpstart.day2.TaskList
+import com.greenfossil.jumpstart.day2.{Task, TaskList}
 import com.greenfossil.data.mapping.Mapping.*
 import com.linecorp.armeria.server.annotation.*
 import com.greenfossil.thorium.{*, given}
 import views.jumpstart.day2.IndexPage
+
+import java.time.LocalDateTime
 
 object HomeController:
 
@@ -14,7 +16,7 @@ object HomeController:
   @Get("/")
   def index =
     val tasks = TaskList.findTasks(_ => true)
-    Ok(IndexPage(tasks, addTaskMapping).render)
+      Ok(IndexPage(tasks, addTaskMapping).render)
 
   @Post("/task/create")
   def createTask = Action{ implicit request =>
@@ -31,11 +33,21 @@ object HomeController:
    * Implement controller action that deletes the task with ID, and redirect to the index page.
    * Expectation: The matching task should no longer be in the list
    */
-  def deleteTask(taskId: Long) = ???
+
+
+  @Delete("/task/delete")
+  def deleteTask(@Param taskId: Long) = Action{ _ =>
+    TaskList.deleteTask(task => task.id == taskId)
+    Redirect(HomeController.index)
+  }
 
   /**
    * Implement controller action that completes the task with ID, and redirect to the index page.
    * Expectation: The matching task should be marked as completed
    */
-  def completeTask(taskId: Long) = ???
+  @Put("/task/complete")
+  def completeTask(@Param taskId: Long) = Action { _ =>
+    TaskList.completeTask(task => task.id == taskId)
+    Redirect(HomeController.index)
+  }
 
--- src/main/scala/views/jumpstart/day2/IndexPage.scala
+++ src/main/scala/views/jumpstart/day2/IndexPage.scala
@@ -1,10 +1,12 @@
 package views.jumpstart.day2
 
 import com.greenfossil.data.mapping.Mapping
-import com.greenfossil.jumpstart.day2.Task
 import com.greenfossil.htmltags.{*, given}
+import com.greenfossil.jumpstart.day2.Task
 import com.greenfossil.thorium.{*, given}
 import controllers.jumpstart.day2.HomeController
+
+import java.time.format.DateTimeFormatter
 
 /**
  * This is to simulate an HTML Page
@@ -18,9 +20,12 @@ case class IndexPage(tasks: List[Task], addTaskMapping: Mapping[String]):
       Tags2.style(
         // For advanced developer, you can add extra CSS style below
         raw(
-          """/*
-            | * Enter your CSS code here (optional)
-            | */
+          """
+            | td { padding: 10px !important; border-right: 1px solid rgba(0,0,0,0.3); border-top: 1px solid rgba(0,0,0,0.5) }
+            | td:nth-child(3) { border-right: none }
+            | td:nth-child(1) { border-top: none }
+            | table { border: 1px solid rgba(0,0,0,0.5) !important }
+            | button { cursor: pointer; background: none; border-radius: 4px; padding: 8px; margin-right: 10px; border: 1px solid  rgba(0,0,0,0.7)  }
             |""".stripMargin)
       )
     ),
@@ -50,16 +55,59 @@ case class IndexPage(tasks: List[Task], addTaskMapping: Mapping[String]):
          *                button > Complete [Marks task as completed]
          *                button > Delete [Deletes task]
          */
+         table(cls := "ui very basic table",
+           tbody(
+             tasks.map{task =>
+               val dateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss")
+               val completeUrl = HomeController.completeTask(task.id).endpoint.url
+               val deleteUrl = HomeController.deleteTask(task.id).endpoint.url
+               tr(
+                 td(cls := "collapsing", task.id),
+                 td(cls := "collapsing", task.description),
+                 td(cls := "collapsing", if (task.isCompleted) s"Completed at ${task.completedOn.map(_.format(dateTimeFormatter)).get}" else "Pending"),
+                 td(cls := "collapsing", s"Created at ${task.created.format(dateTimeFormatter)}"),
+                 td(cls := "collapsing",
+                   button("Complete", onclick := s"completeTask('$completeUrl')"),
+                   button("Delete", onclick := s"deleteTask('$deleteUrl')"),
+                 ),
+               )
+             }
+           )
+         )
+
       ),
 
       script(tpe := "text/javascript", src := "https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.min.js"),
       script(tpe := "text/javascript", src := "https://cdn.jsdelivr.net/npm/fomantic-ui@2.9.3/dist/semantic.min.js"),
       script(tpe := "text/javascript",
         raw(
-          """ /*
-            |  *  Enter JavaScript code here
-            |  */
-            |""".stripMargin)
+          s"""
+           function completeTask(url){
+               const xhr = new XMLHttpRequest();
+               xhr.open('PUT', url);
+               xhr.onload = () => {
+               if(xhr.status === 200) {
+                 console.log('completed the task')
+                 location.reload()
+                } else {
+                  alert('bad thing happened')
+                }
+               }
+                xhr.send();
+            }
+           function deleteTask(url){
+             const xhr = new XMLHttpRequest();
+             xhr.open('DELETE', url);
+             xhr.onload = () => {
+             if(xhr.status === 200) {
+                 console.log('deleted the task')
+                 location.reload()
+              } else {
+                alert('bad thing happened')
+              }
+            }
+             xhr.send();
+           }""")
       )
     )
   )
